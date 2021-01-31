3605e044021702accca343487bd5541615c2d404
Sylvester Henrique <sylvester.henrique@unidesk.com.br>
2021-01-30 22:06:05 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace AzureFileUploader
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var path = configuration["UploadAbsolutePath"];
+            var tempPath = Path.Combine(path, @"..\AzureFileUploaderTemp.zip");
+            var connectionString = Environment.GetEnvironmentVariable(configuration["Azure:ConnectionStringEnvironmentVariableName"]);
+            var azureBlobContainer = configuration["Azure:BlobContainer"];
+            var azureBlobNamePrefix = string.IsNullOrWhiteSpace(configuration["Azure:BlobNamePrefix"]) ? string.Empty : configuration["Azure:BlobNamePrefix"] + "_";
+            var azureBlobName = azureBlobNamePrefix + DateTime.Now.ToString(@"yyyy-MM-dd_hh\h-mm\m") + ".zip";
+
+            var beginTime = DateTime.Now;
+
+            Console.WriteLine("Compressing files...");
+            if (File.Exists(tempPath))
+                File.Delete(tempPath);
+
+            ZipFile.CreateFromDirectory(path, tempPath, CompressionLevel.Fastest, includeBaseDirectory: false);
+            Console.WriteLine("Successfully compressed files.");
+            var compressTime = DateTime.Now - beginTime;
+            Console.WriteLine($"Compress time: {compressTime.ToString(@"hh\:mm\:ss")}");
+
+            var blobContainerClient = new BlobContainerClient(connectionString, azureBlobContainer);
+            var blobClient = blobContainerClient.GetBlobClient(azureBlobName);
+
+            var fileInfo = new FileInfo(tempPath);
+            var progress = new Progress(fileInfo.Length, pace: 1);
+
+            var beginUpload = DateTime.Now;
+            Console.WriteLine();
+            Console.WriteLine("Uploading...");
+            using (var uploadFileStream = fileInfo.OpenRead())
+            {
+                await blobClient.UploadAsync(uploadFileStream, progressHandler: progress);
+            }
+            fileInfo.Delete();
+            Console.WriteLine();
+            Console.WriteLine("Successfully uploaded files.");
+
+            var endTime = DateTime.Now;
+            var uploadTime = endTime - beginUpload;
+            var totalTime = endTime - beginTime;
+            Console.WriteLine($"Upload time: {uploadTime.ToString(@"hh\:mm\:ss")}");
+            Console.WriteLine($"Total time:  {totalTime.ToString(@"hh\:mm\:ss")}");
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Progress.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace AzureFileUploader
+{
+    class Progress : IProgress<long>
+    {
+        private readonly long fileLength;
+        private readonly int pace;
+        private readonly char progressBarBeginChar = '(';
+        private readonly char progressBarPaceNotReachedChar = '|';
+        private readonly char progressBarPaceReachedChar = '-';
+        private readonly char progressBarEndChar = ')';
+
+        private StringBuilder progressBar;
+        private int lastPercentage;
+        private int currentPace;
+
+        public Progress(long fileLength, int pace = 5)
+        {
+            if (pace < 1 || pace > 100)
+                throw new ArgumentOutOfRangeException($"{nameof(pace)} must be between 1 and 100");
+
+            this.pace = pace;
+            this.fileLength = fileLength;
+
+            var progressBarDrawing = new string(Enumerable.Range(0, (100 / pace)).Select(i => progressBarPaceNotReachedChar).ToArray());
+            progressBar = new StringBuilder(progressBarBeginChar + progressBarDrawing + progressBarEndChar);
+        }
+
+        public void Report(long value)
+        {
+            if (value == 0)
+            {
+                currentPace = 0;
+                Console.WriteLine(progressBar);
+            }
+
+            var percentage = (int)((value / (double)fileLength) * 100);
+            if (percentage > 0 && percentage != lastPercentage && percentage % pace == 0)
+            {
+                currentPace++;
+                PrintProgress(currentPace);
+            }
+            lastPercentage = percentage;
+        }
+
+
+        private void PrintProgress(int currentPace)
+        {
+            if (currentPace != 0)
+                ClearCurrentConsoleLine();
+
+            var progressBarNow = progressBar.Replace(progressBarPaceNotReachedChar, progressBarPaceReachedChar, 0, currentPace + 1).ToString();
+            Console.WriteLine(progressBarNow);
+        }
+
+        public static void ClearCurrentConsoleLine()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+    }
+}
